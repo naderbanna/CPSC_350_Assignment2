f2db1f14c4285218d41808a23682e976efc70740
naderbanna <banna@chapman.edu>
2020-10-10 05:25:16 +0000

    changes in main & readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# CPSC_350_Assignment2
+# CPSC_350_Assignment1
+Nader Banna
+2291299
+banna@chapman.edu
+CPSC 231-02
+A2
+
+
+Files
+- main.cpp
+- Classicmode.cpp
+- Doughnutmode.cpp
+- Mirrormode.cpp
+- Classicmode.h
+- Doughnutmode.h
+- Mirrormode.h
+- Makefile
+- assign2-sampleFile2.txt
+- testFiles folder
+
+Known Errors
+- simulation does not stop when world has stabilized
+- simulation does not continue until infinity if world is not stabilized
+- simulation is hard coded to 35 generations
+
+
+References
+- https://www.hellgeeks.com/two-dimensional-arrays/
+- http://www.cplusplus.com/doc/tutorial/basic_io/
+- https://stackoverflow.com/questions/23444844/c-error-terminate-called-after-throwing-an-instance-of-stdbad-alloc?rq=1
+- https://stackoverflow.com/questions/14430487/c-deleting-array-in-destructor
+- https://stackoverflow.com/questions/21257544/c-wait-for-user-input
+- https://stackoverflow.com/questions/7010760/c-keypress-getch-cin-get
+- https://stackoverflow.com/questions/56552148/how-to-fix-terminate-called-after-throwing-an-instance-of-stdlogic-error-wha
--- main.cpp
+++ main.cpp
@@ -10,14 +10,16 @@ using namespace std;
 int main(int argc, char** argv){
   bool program = true;
   while(program){
+    cout << "Hello..." << endl << "What would you like to: " << endl << "1. Start with random Configuration" << endl << "2. Specify a flat-file configuration" << endl << "3. End Program" << endl;
     string gridType = "";
-    cout << "Hello..." << endl << "What would you like to: " << endl << "1. Start with random Configuration" << endl << "2. Specify a flat-file configuration" << endl;
-    getline(cin, gridType);
+    cin >> gridType;
+    //getline(cin, gridType);
 
     if(gridType == "1"){
+      cout << "random grid" << endl;
       string boundary = "";
       cout << "Enter the Boundary Mode: " << endl << "1. Classic" << endl << "2. Doughnut" << endl << "3. Mirror" << endl;
-      getline(cin, boundary);
+      cin >> boundary;
       if(boundary == "1"){
 
         cout << "Classic Mode Output" << endl << "___________________" << endl;
@@ -32,27 +34,30 @@ int main(int argc, char** argv){
         Mirrormode *test = new Mirrormode();
       }
 
-    }if(gridType == "2"){
-      string inputFile = "";
-      string boundary = "";
+      }if(gridType == "2"){
+        string inputFile = "";
+        string boundary = "";
 
-      cout << "Enter a file name: ";
-      getline(cin, inputFile);
-      cout << "Enter the Boundary Mode: " << endl << "1. Classic" << endl << "2. Doughnut" << endl << "3. Mirror" << endl;
-      getline(cin, boundary);
-      if(boundary == "1"){
-        cout << "Classic Mode Output" << endl << "___________________" << endl;
-        Classicmode *test = new Classicmode(inputFile);
+        cout << "Enter a file name: " << endl;
+        cin >> inputFile;
+        cout << "Enter the Boundary Mode: " << endl << "1. Classic" << endl << "2. Doughnut" << endl << "3. Mirror" << endl;
+        cin >> boundary;
+        if(boundary == "1"){
+          cout << "Classic Mode Output" << endl << "___________________" << endl;
+          Classicmode *test = new Classicmode(inputFile);
 
-      }if(boundary == "2"){
-        cout << "Doughnut Mode Output" << endl << "___________________" << endl;
-        Doughnutmode *test = new Doughnutmode(inputFile);
+        }if(boundary == "2"){
+          cout << "Doughnut Mode Output" << endl << "___________________" << endl;
+          Doughnutmode *test = new Doughnutmode(inputFile);
 
-      }if(boundary == "3"){
-        cout << "Mirror Mode Output" << endl << "___________________" << endl;
-        Mirrormode *test = new Mirrormode(inputFile);
+        }if(boundary == "3"){
+          cout << "Mirror Mode Output" << endl << "___________________" << endl;
+          Mirrormode *test = new Mirrormode(inputFile);
+        }
+      }if(gridType == "3"){
+        break;
       }
-    }
+
 }
   return 0;
 };
